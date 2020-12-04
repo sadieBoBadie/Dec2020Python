17e31b37273522387b922997aca5d687d955199e
sadieBoBadie <sflick@codingdojo.com>
2020-12-03 16:37:19 -0800

    Day 5 Algo prompts

---
+++ Week1/Day5/w1day5_algos/invertObj.js
@@ -0,0 +1,23 @@
+/* 
+  Invert Hash
+  A hash table / hash map is an obj / dictionary
+  Given an object / dict,
+  return a new object / dict that has the keys and the values swapped so that the keys become the values and the values become the keys
+*/
+
+const obj1 = { name: "Zaphod", charm: "high", morals: "dicey" };
+const expected1 = { Zaphod: "name", high: "charm", dicey: "morals" };
+
+/**
+ * Inverts the given object's key value pairs so that the original values
+ * become the keys and the original keys become the values.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Object<string, string>} obj An object with string keys and string values.
+ * @return The given object with key value pairs inverted.
+ */
+function invertObj(obj) {}
+
+module.exports = { invertObj };
+
+/*****************************************************************************/
