64774b498ae2c2b99b326411f2d5938f2bd66eb0
sadieBoBadie <sflick@codingdojo.com>
2020-12-06 20:49:59 -0800

    Week 2 Day 1 Algos and Session Debug Activity

---
+++ Week2/day1/reverseWordOrder.js
@@ -0,0 +1,21 @@
+/* 
+  Reverse Word Order
+  Given a string of words (with spaces)
+  return a new string with words in reverse sequence.
+*/
+
+const str1 = "This is a test";
+const expected1 = "test a is This";
+
+/**
+ * Reverses the order of the words but not the words themselves form the given
+ * string of space separated words.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} wordsStr A string containing space separated words.
+ * @return {string} The given string with the word order reversed but the words
+ *    themselves are not reversed.
+ */
+function reverseWordOrder(wordsStr) {}
+
+module.exports = { reverseWordOrder };
---
+++ Week2/day1/session_debug_BROKEN/session_debug_project/leaderboard/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request): 
+
+    return render(request, "index.html")
+
+
+def leaderBoard(request):
+
+    if "user_name" not in session:
+        print('redirecting...')
+        return redirect('/')
+
+    ranks = ["first", "second", "third"]
+
+    for key in ranks:
+        if key not in request.session:
+            request.session[key] = 'Please assign a rank.'
+
+    return render(request, "leaderboard.html")
+
+def show(rank):
+
+    name = ""
+
+    if rank == 1:
+        if request.session['first'] == "Please assign a rank.":
+            return redirect('/dashboard')
+        name = request.session['first']
+    elif rank == 2:
+        name = request.session['second']
+    else:
+        name = request.session['third']
+
+    return render(request, "showFriend.html", {'rank':rank, 'name': name })
+
+def enter(request):
+    print(request.POST)
+    name = request.POST["FirstName"] + " " + request.POST["Last_Name"]
+    request.session['user_name'] = name
+    return redirect('/leaderboard')
+
+def changeRanks(request):
+
+    print(request.POST)
+
+    request.session['first'] = request.POST['first']
+    request.session['second'] = request.POST['second']
+    request.session['third'] = request.POST['third']
+
+    return redirect('/leaderboard')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
