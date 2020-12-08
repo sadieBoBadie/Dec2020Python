8a24453541085590a338c6d2b68c33f1854acbf1
sadieBoBadie <sflick@codingdojo.com>
2020-12-08 09:08:04 -0800

    week2 day2 additions

---
+++ Week2/day2/reverseWords.js
@@ -0,0 +1,29 @@
+/* 
+  Given a string containing space separated words
+  Reverse each word in the string.
+  If you need to, use .split to start, then try to do it without.
+*/
+
+const str1 = "hello";
+const expected1 = "olleh";
+
+const str2 = "hello world";
+const expected2 = "olleh dlrow";
+
+const str3 = "abc def ghi";
+const expected3 = "cba fed ihg";
+
+/**
+ * Reverses the letters in each words in the given space separated
+ * string of words. Does NOT reverse the order of the words themselves.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str Contains space separated words.
+ * @return {string} The given string with each word's letters reversed.
+ */
+function reverseWords(str) {}
+
+module.exports = { reverseWords };
+
+/*****************************************************************************/
+
