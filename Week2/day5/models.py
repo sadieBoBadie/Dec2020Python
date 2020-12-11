978282982fd540bb978361ba33abf5b560f05042
sadieBoBadie <sflick@codingdojo.com>
2020-12-11 10:37:49 -0800

    group exercise

--- Week2/day5/models.py
+++ Week2/day5/models.py
@@ -1,5 +1,15 @@
-# Create the model for a Dungeons and Dragons sign up
+# 1
+# Create the models.py for a Dungeons and Dragons sign up
 
 # Users can create campaigns as the Dungeon Master
 # Users can join campaigns as players
 # Campaigns can have many players
+
+
+
+
+# 2
+# Create the models.py for a book lending app
+
+# Users can post books to lend
+# Users can borrow books from other users.
