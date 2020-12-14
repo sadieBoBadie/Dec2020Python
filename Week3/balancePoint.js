a0a8e65cd83b4d66fa59d4b66bb880fa93162c92
sadieBoBadie <sflick@codingdojo.com>
2020-12-14 08:55:20 -0800

    week3 day 1 algos

---
+++ Week3/balancePoint.js
@@ -0,0 +1,27 @@
+/* 
+  Balance Point
+  Write a function that returns whether the given
+  array has a balance point BETWEEN indices, 
+  where one side’s sum is equal to the other’s. 
+*/
+
+const nums1 = [1, 2, 3, 4, 10];
+const expected1 = true;
+// Explanation: between indices 3 & 4
+
+const nums2 = [1, 2, 4, 2, 1];
+const expected2 = false;
+
+/**
+ * Determines if there is a balance point BETWEEN indexes where the sum of the
+ *    left side is equal to the sum of the right side of the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @return {boolean} Indicates if a balance point exists.
+ */
+function balancePoint(nums) {}
+
+module.exports = { balancePoint };
+
+/*****************************************************************************/
